fa67c0bc7a72f9615af6bb5b2a01adfd87d065fe
IvanHapo <ivanhaponouk@gmail.com>
2022-03-20 20:28:03 -0300

    Desafio Libreria

---
+++ script.js
@@ -0,0 +1,5 @@
+//Es basico, la idea es que el usuario ya este registrado e inicie sin fallo
+
+document.getElementById('buttonLogin').addEventListener('click', () =>{
+    swal("Bienvenido", "Inició sesión con Exito", "success");
+})
